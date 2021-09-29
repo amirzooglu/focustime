73cee4125157f7f106b2767fe6b5164ab7e7fb43
Ahmet Mirzooglu <ahmetmirzooglu@gmail.com>
2021-09-29 13:57:18 +0300

    trial

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Platform } from 'react-native';
-import Constants from 'expo-constants';
-import { Focus } from './src/features/focus/Focus';
-import { Timer } from './src/features/timer/Timer';
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Platform } from "react-native";
+import Constants from "expo-constants";
+import { Focus } from "./src/features/focus/Focus";
+import { Timer } from "./src/features/timer/Timer";
 // or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
-import { colors } from './src/utils/colors';
-import { fontSizes, spacing } from './src/utils/sizes';
+import { Card } from "react-native-paper";
+import { colors } from "./src/utils/colors";
+import { fontSizes, spacing } from "./src/utils/sizes";
 
-import { FocusHistory } from './src/features/focus/FocusHistory';
+import { FocusHistory } from "./src/features/focus/FocusHistory";
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const STATUSES = {
   Complete: 1,
@@ -18,7 +18,7 @@ const STATUSES = {
 };
 
 export default function App() {
-  const [focusSubject, setFocusSubject] = useState('');
+  const [focusSubject, setFocusSubject] = useState("");
   const [focusHistory, setFocusHistory] = useState([]);
 
   const addFocusHistorySubjectWithState = (subject, status) => {
@@ -31,7 +31,7 @@ export default function App() {
 
   const saveFocusHistory = async () => {
     try {
-      await AsyncStorage.setItem('focusHistory', JSON.stringify(focusHistory));
+      await AsyncStorage.setItem("focusHistory", JSON.stringify(focusHistory));
     } catch (e) {
       console.log(e);
     }
@@ -39,7 +39,7 @@ export default function App() {
 
   const loadFocusHistory = async () => {
     try {
-      const history = await AsyncStorage.getItem('focusHistory');
+      const history = await AsyncStorage.getItem("focusHistory");
       if (history && JSON.parse(history).length)
         setFocusHistory(JSON.parse(history));
     } catch (e) {
@@ -60,11 +60,11 @@ export default function App() {
           focusSubject={focusSubject}
           onTimerEnd={() => {
             addFocusHistorySubjectWithState(focusSubject, STATUSES.Complete);
-            setFocusSubject('');
+            setFocusSubject("");
           }}
           clearSubject={() => {
             addFocusHistorySubjectWithState(focusSubject, STATUSES.Cancelled);
-            setFocusSubject('');
+            setFocusSubject("");
           }}
         />
       ) : (
@@ -81,7 +81,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: Platform.OS === 'ios' ? spacing.md : spacing.lg,
+    paddingTop: Platform.OS === "ios" ? spacing.md : spacing.lg,
     backgroundColor: colors.darkBlue,
   },
 });
